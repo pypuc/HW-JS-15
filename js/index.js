951906f47af33e4e9fc09f8233e4a4cb60d68c9d
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2025-01-05 09:57:05 +0200

    work

---
+++ js/index.js
@@ -0,0 +1,29 @@
+// 1
+const users = [
+    {name: "user1", balance: 395, location: "Ukraine"},
+    {name: "user2", balance: 57, location: "England"},
+    {name: "user3", balance: 917, location: "Austria"},
+    {name: "user4", balance: 1020, location: "Switzerland"},
+    {name: "user5", balance: 150, location: "Ukraine"}
+];
+const someBalances = users.reduce((acc, user) => acc + user.balance, 0);
+console.log(someBalances);
+
+// 2
+
+const visitors = [
+    {name: "Artem", balance: 395, location: "Ukraine", friend: "Vanya"},
+    {name: "Sasha", balance: 57, location: "Ukraine", friend: "Vanya"},
+    {name: "Lisa", balance: 917, location: "Ukraine", friend: "Vanya"},
+    {name: "Dima", balance: 1020, location: "Ukraine", friend: "Alexei"},
+    {name: "Alexei", balance: 150, location: "Ukraine", friend: "Dima"}
+];
+const filterVisitors = visitors.reduce((newVisitors, visitor) => {
+    if (visitor.friend === "Vanya") {
+        newVisitors.push(visitor);
+    }
+    return newVisitors;
+}, []);
+console.log(filterVisitors);
+
+// 3
