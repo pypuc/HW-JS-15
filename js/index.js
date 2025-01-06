9c4961d56938841a83d68593f7c2546e30152d15
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2025-01-06 17:44:56 +0200

    fix

--- js/index.js
+++ js/index.js
@@ -27,3 +27,25 @@ const filterVisitors = visitors.reduce((newVisitors, visitor) => {
 console.log(filterVisitors);
 
 // 3
+
+const people = [
+    {name: "Богдан", frriend: "Андрій"},
+    {name: "Даня", frriend: "Дмитро"},
+    {name: "Єгор", frriend: "Андрій"},
+    {name: "Денис", frriend: "Даня"},
+    {name: "Дмитро", frriend: "Даня"},
+    {name: "Андрій", frriend: "Дмитро"}
+];
+const friends = people.reduce((firstFriend, man) => {
+    if (man.frriend === "Андрій") {
+        firstFriend.push(man)
+        return firstFriend
+    } else if (man.frriend === "Дмитро") {
+        firstFriend.push(man)
+        return firstFriend
+        
+    } else if (man.frriend === "Даня") {
+        firstFriend.push(man)
+        return firstFriend
+    }
+})
